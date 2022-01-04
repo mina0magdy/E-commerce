05094c8e6a950066919df38005020d2652ed8106
mina0magdy <92601097+mina0magdy@users.noreply.github.com>
2022-01-04 21:21:48 +0200

    final

--- myApp/app.js
+++ myApp/app.js
@@ -161,7 +161,7 @@ async function addToCart(User,product){
     let foundUser=await FindUser1(User);
     let userCart=foundUser.cart;
     if(userCart.length != 0 && userCart.includes(product)){
-        res.send('<script>alert("the product is already in the card");window.location.href = "/cart" ; </script>');
+        res.send('<script>alert("the product is already in the card"); window.location.href = "/cart" ; </script>');
         //console.log("added")
 
         //alert("the product is already in the card");
@@ -171,7 +171,7 @@ async function addToCart(User,product){
     console.log(userCart);
     try {
         await mongoClient.db('Project_dp').collection('Users').updateOne({'UserName':User.UserName},{$set: {'cart':userCart}});
-        res.send('<script>alert("The Product is added to the Card");window.location.href = "/cart" ; </script>');
+        res.send('<script>alert("The Product is added to the Card"); window.location.href = "/cart" ; </script>');
         //console.log("erroe")
         //alert("The Product is added to the Card");
         await mongoClient.close();
